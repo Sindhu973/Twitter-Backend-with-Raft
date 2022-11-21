953359a5d5af3ffa4ad90930031e9869776d1be8
anubis-robot <anubis@anubis-lms.io>
2022-11-21 23:04:35 +0000

    Anubis Cloud IDE Autosave netid=sb8019

--- web/cmd/auth/auth_test.go
+++ web/cmd/auth/auth_test.go
@@ -1,16 +1,4 @@
 package auth
 
-import(
-	"testing"
-)
-
-func TestRegister(t *testing.T) {
-	user := "testuser"
-	password := "password"
-
-	err = auth.Register(user, password)
-	if err != nil {
-		t.Error(err)
-	}
-}
+package auth
 
